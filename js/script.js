ce29e7ec8f29d63775947d248888e67004c1508b
Peter <peterkimm35@gmail.com>
2022-05-05 13:47:10 -0700

    work in progress

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert("javascript is linked");
+// how to request data from an API using javaScript and jQuery
+
+// There are many ways to do this!
+// 1. jQuerys $.ajax() function
+// 2. The Brower's fetch() function
+// 3. Using the Axios hhtp client library
+
+const URL = "http://www.omdbapi.com/?apikey=a71b148a&t=Die+Hard";
+
+function handleGetData() {
+    
+    $.ajax(URL).then(function (data) {
+        console.log('movie data is ready')
+        console.log(data)
+    }, function (error) {
+        console.log('something is wrong')
+        console.log(error)
+    })
+}
