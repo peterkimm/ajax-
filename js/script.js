13c5c36acd7644082be0d88feafbda6a3e62952d
Peter <peterkimm35@gmail.com>
2022-05-06 07:48:10 -0700

    able to display movie poster

--- js/script.js
+++ js/script.js
@@ -5,13 +5,33 @@
 // 2. The Brower's fetch() function
 // 3. Using the Axios hhtp client library
 
+// VARIABLES
+
 const URL = "http://www.omdbapi.com/?apikey=a71b148a&t=Die+Hard";
 
+// ELEMENT REFERENCES
+
+const $title = $('#title');
+const $year = $('#year');
+const $rated = $('#rated');
+
+
+//EVENT LISTENERS
+
+
+
+
+// FUNCTIONS
+
 function handleGetData() {
-    
+
     $.ajax(URL).then(function (data) {
         console.log('movie data is ready')
-        console.log(data)
+        // console.log(data)
+        $title.text(data.Title)
+        $year.text(data.Year)
+        $rated.text(data.Rated)
+        $('main').append(`<img src="${data.Poster}"/>`)
     }, function (error) {
         console.log('something is wrong')
         console.log(error)
